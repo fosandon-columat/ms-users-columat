60465c9c6e94b943ea753c95f65c74e205fdf565
fosandon-columat <fosandon@columat.com>
2026-02-10 10:24:17 +0100

    Refactor code structure for improved readability and maintainability

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,7 +5,7 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   
-  NATS_SERVERS: string[];
+  // NATS_SERVERS: string[];
 
   JWT_SECRET: string;
 }
@@ -13,14 +13,14 @@ interface EnvVars {
 const envsSchema = joi.object({
   PORT: joi.number().required(),
   
-  NATS_SERVERS: joi.array().items( joi.string() ).required(),
+  // NATS_SERVERS: joi.array().items( joi.string() ).required(),
   JWT_SECRET: joi.string().required(),
 })
 .unknown(true);
 
 const { error, value } = envsSchema.validate({ 
   ...process.env,
-  NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+  // NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
 });
 
 
@@ -34,7 +34,7 @@ const envVars:EnvVars = value;
 export const envs = {
   port: envVars.PORT,
 
-  natsServers: envVars.NATS_SERVERS,
+  // natsServers: envVars.NATS_SERVERS,
 
   jwtSecret: envVars.JWT_SECRET,
 };
